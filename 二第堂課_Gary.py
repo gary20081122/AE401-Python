382de24564d25d5d942ff0308b9b3bf2b3b590d9
gary20081122 <65432772+gary20081122@users.noreply.github.com>
2020-05-16 12:07:24 +0800

    第二堂課

---
+++ 二第堂課_Gary.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 11:03:07 2020
+
+@author: User
+"""
+
+w=9
+x=2
+print(w/x)
+print(w%x)
+print(w//x)
+print(w**x)
+a=3
+b=1
+print('a+b')
+
+p=10
+p+1
+print(p)
+
+d=input('input')
+d=int(d)
+print(d/2)
+
+
+score=input("分數為")
+print(type(score))
+
+score=int(score)
+print(type(score))
+
+if(score>=60):
+    print("pass")
+elif(score ==70):
+    print("在加油")
+else:
+    print("fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
