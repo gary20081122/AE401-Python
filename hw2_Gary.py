9f86dd5e3f8cf5a5b21468503a96c3092317cfe9
gary20081122 <65432772+gary20081122@users.noreply.github.com>
2020-05-23 10:43:15 +0800

    Add files via upload

---
+++ hw2_Gary.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 11:03:07 2020
+
+@author: User
+"""
+
+
+math=input('70')
+eng=input('90')
+math=int(math)
+eng=int(eng)
+if (math >=90 and eng >=90):
+   print('得到獎品')
+elif (math <60 and eng <60):
+    print('要處罰')
+elif (math < 60 or eng <60):
+     print('再加油')
+else:
+    print('pass')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
