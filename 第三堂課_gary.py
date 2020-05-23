4eead103703ff6b60b7d240a62d0343e58024b2a
gary20081122 <65432772+gary20081122@users.noreply.github.com>
2020-05-23 12:10:12 +0800

    Add files via upload

---
+++ 第三堂課_gary.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 11:59:09 2020
+
+@author: User
+"""
+
+
+import random
+x=random.randint(1,10)
+
+count =0
+while count <3:
+    
+    input = input('please guess a number between1~10:')
+    input = int(input)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
