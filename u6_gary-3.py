d6376cae7a45da2d226a72839c33a1e18c173ec2
gary20081122 <65432772+gary20081122@users.noreply.github.com>
2020-06-13 12:01:23 +0800

    Add files via upload

---
+++ u6_gary-3.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 11:41:10 2020
+
+@author: User
+"""
+
+import turtle
+alex=turtle.Turtle()
+
+def draw_square(unit):
+    for i in range(4):
+        alex.forward(unit)
+        alex.left(90)
+        
+draw_square(10)
+draw_square(15)
+draw_square(30)
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
