0c2f4d1ccca386b98cb4946a636d172156460f23
gary20081122 <65432772+gary20081122@users.noreply.github.com>
2020-06-13 12:02:01 +0800

    Add files via upload

---
+++ u6_gary-4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 11:55:05 2020
+
+@author: User
+"""
+
+# 將三個整組相加並回傳
+def add3(a, b, c):
+    answer =a+b+c
+    return answer
+
+#呼叫 add3
+a1 = add3(1,2,3)
+a2 =add3(3,6,9)    
+a3 =add3(12,45,78)
+print(a1, a2, a3)
