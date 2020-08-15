0bd7aa52894e12ef56e4f7d59b17408babf8ce8c
gary20081122 <65432772+gary20081122@users.noreply.github.com>
2020-08-15 11:25:44 +0800

    Add files via upload

---
+++ multiplicationtable_gary.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 11:12:17 2020
+
+@author: User
+"""
+
+
+import random 
+for i in range(1,10):
+    for j in range(1,10):
+        print(i,"x",j,"=",i*j)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
